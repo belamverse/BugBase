eff1334caafee5a1cb81e88ca6963c3197d903be
bmuia <belammuia0@gmail.com>
2025-08-08 19:34:11 +0300

    refractor(landingpage) remove the auth buttons replacing with a custom button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import LandingPage from './pages/landingpage/LandingPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Auth from './components/auth/Auth'
 function App() {
   return (
     <div>
-      <LandingPage />
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
